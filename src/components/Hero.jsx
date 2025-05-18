b9dd699df579c30a9d8eb3fb136720560fbd7c31
vijaysingh7878 <vijaysinghshekhawat9928@gmail.com>
2025-05-18 17:05:16 +0530

    api fatch

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,44 +1,64 @@
 "use client"
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import axiosInstance from './config/axiosInstace';
 export default function Hero() {
-    const images = [
-        '/house.jpg',
-        '/house1.jpg',
-        '/house2.jpg',
-        '/house3.jpg',
-        '/house4.jpg',
-        '/house6.jpg',
-      ];
-      const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    useEffect(() => {
-      const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  const [images, setImage] = useState([])
+
+  // view banner part
+  const view_Banner = async () => {
+    try {
+      const response = await axiosInstance.get('/banner/read');
+      setImage(response.data.allBanner)
+
+    } catch (error) {
+      console.log(error);
+
+    }
+  }
+
+  const All_Banner = images?.filter(data => data.isPublished == true || [])
+
+  useEffect(
+    () => {
+      view_Banner()
+    }, []
+  )
+
+  useEffect(() => {
+    if (!All_Banner || All_Banner.length === 0) return;
+
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % All_Banner.length);
     }, 3000);
 
-    return () => clearInterval(interval); 
-  }, []);
+    return () => clearInterval(interval);
+  }, [All_Banner]);
 
-    return (
-      <section
-        className="mt-20 bg-cover bg-center h-[60vh] md:h-[60vh] flex flex-col items-center justify-center relative"
-        style={{ backgroundImage: `url('${images[currentIndex]}')` }}
-      >
-        
-        <div className="text-white text-center mt-5 md:mt-20 flex flex-col md:flex-row z-10 mb-6">
-          <div className="bg-[#8a98ad] p-3 opacity-85">
-            <h2 className="text-4xl font-bold">Luxury</h2>
-          </div>
-          <div className="bg-[#BFA249] p-3 opacity-85">
-            <h2 className="text-4xl font-bold">Property in Jaipur</h2>
-          </div>
+
+  return (
+    <section
+      className="mt-20 w-full bg-cover bg-center bg-no-repeat h-[60vh] flex flex-col items-center justify-center relative"
+      style={{
+        backgroundImage: `url('${All_Banner[currentIndex]?.bannerImage || "/placeholder.jpg"}')`,
+      }}
+    >
+
+      <div className="text-white text-center mt-5 md:mt-20 flex flex-col md:flex-row z-10 mb-6">
+        <div className="bg-[#8a98ad] p-3 opacity-85">
+          <h2 className="text-4xl font-bold">Luxury</h2>
         </div>
-  
-       
-        <div className="bg-white bg-opacity-90 px-8 py-6 opacity-85 rounded shadow-lg z-10">
-           <div className="text-center mb-4">
-           <h1 className="text-2xl font-bold text-[#8a98ad]">Discover Your Dream Home</h1>
-           </div>
+        <div className="bg-[#BFA249] p-3 opacity-85">
+          <h2 className="text-4xl font-bold">Property in Jaipur</h2>
+        </div>
+      </div>
+
+
+      <div className="bg-white bg-opacity-90 px-8 py-6 opacity-85 rounded shadow-lg z-10">
+        <div className="text-center mb-4">
+          <h1 className="text-2xl font-bold text-[#8a98ad]">Discover Your Dream Home</h1>
+        </div>
         <div className=" grid  grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
           <select className="w-full px-4 py-2 rounded border focus:outline-none outline-1 outline-[#BFA249]" >
             <option>Property For Sale</option>
@@ -62,13 +82,12 @@ export default function Hero() {
             <option>Price Low to High</option>
             <option>Price High to Low</option>
             <option>Date Old to New</option>
-            </select>
+          </select>
           <button className=" w-full bg-[#BFA249] text-white px-14 py-2 rounded hover:bg-[#a88e37]">
             Search
           </button>
         </div>
-        </div>
-      </section>
-    );
-  }
-  
+      </div>
+    </section>
+  );
+}
